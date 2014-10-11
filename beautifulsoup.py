1f2283cd6be811500ece2efb1f1a6ef3b2c98a4b
Sophie Gershon <sophgersh@gmail.com>
2014-10-10 21:34:09 -0400

    put in urllib2 to get web page, works to remove html tags (css and extra stuff not removed)

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,10 +1,16 @@
 from bs4 import BeautifulSoup
+import urllib2
 
-def getText(doc):
-    f=open(doc,'r')
-    s=f.read()
-    f.close()
-    soup = BeautifulSoup(s)
+
+def getText(url):
+    s = urllib2.urlopen(url)
+    data = s.read()
+    s.close()
+    print s
+    soup = BeautifulSoup(data)
     return soup.get_text()
 
-print getText("sample.html")
+    
+print getText("http://love-python.blogspot.com/2008/02/get-html-source-of-url.html")
+
+
