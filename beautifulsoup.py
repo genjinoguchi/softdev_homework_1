1d7f70245cc2a14000c0742556ba9ef24b1810ba
Sophie Gershon <sophgersh@gmail.com>
2014-10-13 14:06:22 -0400

    list function

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,16 +1,23 @@
 from bs4 import BeautifulSoup
 import urllib2
 
+def listTexts(urls):
+    htmls = []
+    for u in urls:
+       htmls.add(getText(url))
+    return htmls 
 
 def getText(url):
     s = urllib2.urlopen(url)
     data = s.read()
     s.close()
-    print s
     soup = BeautifulSoup(data)
     return soup.get_text()
 
-    
-print getText("http://love-python.blogspot.com/2008/02/get-html-source-of-url.html")
-
-
+if __name__ == '__main__':    
+    #print getText("https://docs.python.org/2/howto/urllib2.html")
+    #print getText("http://www.nytimes.com/2009/12/21/us/21storm.html?_r=0")
+    # 303 error
+    print getText("http://screenrant.com/iron-man-actor-mark-wahlberg")
+    # has javascript,css,etc 
+   
